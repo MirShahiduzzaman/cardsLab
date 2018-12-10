095faa69f21dc214014924293799db76a5c52f73
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-12-10 10:08:13 -0500

    Made Card Class.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

---
+++ src/Card.java
@@ -0,0 +1,45 @@
+public class Card
+{
+    String rank, suit;
+    int pointValue;
+
+    public Card(String rank, String suit, int pointValue)
+    {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getPointValue() {
+        return pointValue;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setPointValue(int pointValue) {
+        this.pointValue = pointValue;
+    }
+
+    public boolean equals(Card otherCard)
+    {
+        if(this.pointValue == otherCard.pointValue)
+        {
+            return true;
+        }
+        return false;
+    }
+}
