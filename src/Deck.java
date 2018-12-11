807409758de82ef948c40b2c971f144fe6f6f64b
Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>
2018-12-11 10:05:40 -0500

    Finished up to deal function in Deck.
    Signed-off-by: Mir Shahiduzzaman <mshahiduzzaman8068@bths.edu>

--- src/Deck.java
+++ src/Deck.java
@@ -8,6 +8,45 @@ public class Deck
 
     public Deck(String[] rank,String[] suit,int[] pointValue)
     {
-        List<Card> deck = new ArrayList<>();
+        unDealt = new ArrayList<>();
+
+        for(int i = 0;i<rank.length;i++)
+        {
+            for(int a = 0;a<rank.length;a++)
+            {
+                for(int b = 0;b<rank.length;b++)
+                {
+                    Card newCard = new Card(rank[i],suit[a],pointValue[b]);
+                    unDealt.add(newCard);
+                }
+            }
+        }
+    }
+
+    public boolean isEmpty()
+    {
+        if(unDealt.size() == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public int size()
+    {
+        return unDealt.size();
+    }
+
+    public Card deal()
+    {
+        if(unDealt.size() == 0)
+        {
+            return null;
+        }
+
+        Card selected = unDealt.get(0);
+        Dealt.add(selected);
+        unDealt.remove(0);
+        return(selected);
     }
 }
